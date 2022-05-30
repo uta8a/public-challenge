56d38fcc768c6e4fd99ce437bf975f3a5726b1ca
Tksi <Tksi@users.noreply.github.com>
2022-05-30 09:35:51 +0900

    COPY package.json

--- web/simple-clicker/build/scripts/loader.mjs
+++
@@ -1,8 +0,0 @@
-let entrypoint = true;
-export async function load(url, context, defaultLoad) {
-  if (entrypoint) {
-    context.format = "module";
-    entrypoint = false;
-  }
-  return defaultLoad(url, context, defaultLoad);
-}
