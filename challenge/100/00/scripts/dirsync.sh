981684495aaad1e387f7f9bab5ff28b84dd81448
Masanori Tani <uta8a@uta8a.org>
2025-05-10 13:17:02 +0900

    delete scripts

--- challenge/100/00/scripts/dirsync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ue
-
-cp -r ./build/public ./files/public
-cp ./build/.gitignore ./files/.gitignore
-cp ./build/index.js ./files/index.js
-cp ./build/package.json ./files/package.json
